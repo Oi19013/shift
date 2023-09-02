81018ebdce9b95054899eaff761541e0d826f454
Oi19013 <oitaku0707@gmail.com>
2023-09-02 13:45:24 +0900

    format

--- app.js
+++ app.js
@@ -1,193 +1,210 @@
-const { render } = require('ejs');
-const express = require('express');
-const mysql = require('mysql');
+const { render } = require("ejs");
+const express = require("express");
+const mysql = require("mysql");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const today = new Date();
 
 // body-parserミドルウェアを使用
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(express.static('public'))
-app.set('views',  'views')
-app.set('view engine', 'ejs')
+app.use(express.static("public"));
+app.set("views", "views");
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-    res.render('menu.ejs');
+app.get("/", (req, res) => {
+  res.render("menu.ejs");
 });
-app.get('/reg.ejs', (req, res) => {
-    res.render('reg.ejs');
+app.get("/reg.ejs", (req, res) => {
+  res.render("reg.ejs");
 });
-app.get('/auth.ejs', (req, res) => {
-    res.render('auth.ejs');
+app.get("/auth.ejs", (req, res) => {
+  res.render("auth.ejs");
 });
-app.get('/main.ejs', (req, res) => {
-    res.render('main.ejs');
+app.get("/main.ejs", (req, res) => {
+  res.render("main.ejs");
 });
-app.get('/api/getCalendar', async(req, res) => {
-    try{
-        const getCalendars = await getShift(req.query.month)
-        // console.log(getCalendars)
-        res.send(getCalendars)
-    } catch (err){
-        console.log(err)
-    }
+app.get("/api/getCalendar", async (req, res) => {
+  try {
+    const getCalendars = await getShift(req.query.month);
+    // console.log(getCalendars)
+    res.send(getCalendars);
+  } catch (err) {
+    console.log(err);
+  }
 });
-app.post('/api/addCalendar', async(req, res) => {
-    try{
-        const params = req.body["params"]
-        await sendShift(params["month"], params["user_name"], params["date"], params["time"]);
-    } catch (err){
-        console.log(err)
-    }
+app.post("/api/addCalendar", async (req, res) => {
+  try {
+    const params = req.body["params"];
+    await sendShift(
+      params["month"],
+      params["user_name"],
+      params["date"],
+      params["time"]
+    );
+  } catch (err) {
+    console.log(err);
+  }
 });
 app.listen(3000);
 
 // もしなければ次の月のtableを作成
-const endDate = new Date(today.getFullYear(), today.getMonth() + 2, 0).getDate();
+const endDate = new Date(
+  today.getFullYear(),
+  today.getMonth() + 2,
+  0
+).getDate();
 const pool = mysql.createPool({
-    user: 'root',
-    password: '',
-    database: 'shift',
+  user: "root",
+  password: "",
+  database: "shift",
 });
-pool.query(`CREATE table IF NOT EXISTS ${today.getFullYear()}_${today.getMonth() + 3} SELECT name from user;`);
+pool.query(
+  `CREATE table IF NOT EXISTS ${today.getFullYear()}_${
+    today.getMonth() + 3
+  } SELECT name from user;`
+);
 // 日毎のカラム作成
-for (let i = 1; i <= endDate; i += 1){
-    pool.query(
-        `DESCRIBE ${today.getFullYear()}_${today.getMonth() + 3} ${i}日;`,
-        (error, results) => {
-            if (results.length == 0) pool.query(`ALTER TABLE ${today.getFullYear()}_${today.getMonth() + 3} ADD ${i}日 varchar(5);`);
-        }
-    )
+for (let i = 1; i <= endDate; i += 1) {
+  pool.query(
+    `DESCRIBE ${today.getFullYear()}_${today.getMonth() + 3} ${i}日;`,
+    (error, results) => {
+      if (results.length == 0)
+        pool.query(
+          `ALTER TABLE ${today.getFullYear()}_${
+            today.getMonth() + 3
+          } ADD ${i}日 varchar(5);`
+        );
+    }
+  );
 }
 
 // ユーザー登録api
 let flg_reg = false;
-app.post('/reg', (req, res) => {
-    const data = req.body;
-    console.log(data)
-    reg(data);
-    setTimeout(function () {
-        if (flg_reg) {
-            res.redirect('/');
-        } else {
-            res.redirect('reg.ejs?msg=miss!');
-            console.log('miss')
-        }
-    }, 1000);
+app.post("/reg", (req, res) => {
+  const data = req.body;
+  console.log(data);
+  reg(data);
+  setTimeout(function () {
+    if (flg_reg) {
+      res.redirect("/");
+    } else {
+      res.redirect("reg.ejs?msg=miss!");
+      console.log("miss");
+    }
+  }, 1000);
 });
 
 // ユーザー認証api
 let flg_auth = false;
-app.post('/auth', (req, res) => {
-    const data = req.body;
-    const user_name = auth(data);
-    setTimeout(function () {
-        if (flg_auth) {
-            res.redirect(`main.ejs?user_name=${user_name}`);
-        } else {
-            res.redirect('auth.ejs?msg=IDまたはパスワードが正しくありません');
-        }
-    }, 1000);
+app.post("/auth", (req, res) => {
+  const data = req.body;
+  const user_name = auth(data);
+  setTimeout(function () {
+    if (flg_auth) {
+      res.redirect(`main.ejs?user_name=${user_name}`);
+    } else {
+      res.redirect("auth.ejs?msg=IDまたはパスワードが正しくありません");
+    }
+  }, 1000);
 });
 
 // ユーザー登録
-function reg (param) {
-    const name = param["name"];
-    const pass = param["pass"];
+function reg(param) {
+  const name = param["name"];
+  const pass = param["pass"];
 
-    // const mysql = require('mysql');
-    const connection = mysql.createConnection({
-        user: 'root',
-        password: '',
-    });
-    connection.query('USE shift')
-    connection.query(
-        'INSERT INTO user (name, pass) VALUES ("' +
-        name +
-        '", "' +
-        pass +
-        '");'
-        , function (err, result, fields) {
-            if (err) {
-                flg_reg = false;
-                return;
-            }
-    });
-    connection.end();
-    flg_reg = true;
-    return;
-
+  // const mysql = require('mysql');
+  const connection = mysql.createConnection({
+    user: "root",
+    password: "",
+  });
+  connection.query("USE shift");
+  connection.query(
+    'INSERT INTO user (name, pass) VALUES ("' + name + '", "' + pass + '");',
+    function (err, result, fields) {
+      if (err) {
+        flg_reg = false;
+        return;
+      }
+    }
+  );
+  connection.end();
+  flg_reg = true;
+  return;
 }
 
 // ユーザー認証
-function auth (param) {
-    const name = param["name"];
-    const pass = param["pass"];
+function auth(param) {
+  const name = param["name"];
+  const pass = param["pass"];
 
-    const connection = mysql.createConnection({
-        user: 'root',
-        password: '',
-    });
-    connection.query('USE shift');
-    connection.query(
-        'SELECT name FROM user WHERE name = "' +
-        name +
-        '" AND pass = "' +
-        pass +
-        '";'
-    , function (err, result, fields) {
-        if (err || !result || result.length == 0 || result.affectedRows == 0 || !result[0] || !result[0].name || result[0].name != name) {
-            flg_auth = false;
-            return;
-        }
-    });
-    connection.end();
-    flg_auth = true;
-    return name;
+  const connection = mysql.createConnection({
+    user: "root",
+    password: "",
+  });
+  connection.query("USE shift");
+  connection.query(
+    'SELECT name FROM user WHERE name = "' +
+      name +
+      '" AND pass = "' +
+      pass +
+      '";',
+    function (err, result, fields) {
+      if (
+        err ||
+        !result ||
+        result.length == 0 ||
+        result.affectedRows == 0 ||
+        !result[0] ||
+        !result[0].name ||
+        result[0].name != name
+      ) {
+        flg_auth = false;
+        return;
+      }
+    }
+  );
+  connection.end();
+  flg_auth = true;
+  return name;
 }
-
 
 // データ取得
-async function getShift (month) {
-    const connection = mysql.createConnection({
-        user: 'root',
-        password: '',
+async function getShift(month) {
+  const connection = mysql.createConnection({
+    user: "root",
+    password: "",
+  });
+  connection.query("USE shift");
+  const shift = await new Promise((resolve, reject) => {
+    connection.query("SELECT * FROM " + month + ";", (error, results) => {
+      return resolve(results);
     });
-    connection.query('USE shift');
-    const shift = await new Promise((resolve, reject) => {
-        connection.query(
-            'SELECT * FROM ' +
-            month +
-            ';',
-            (error, results) => {
-                return resolve(results);
-            }
-        );
-    });
-    connection.end();
-    return shift;
+  });
+  connection.end();
+  return shift;
 }
 
-
 // データ送信
-async function sendShift (month, name, date, time) {
-    const connection = mysql.createConnection({
-        user: 'root',
-        password: '',
-    });
-    connection.query('USE shift');
-    const shift = await new Promise((resolve, reject) => {
-        connection.query(
-            "UPDATE `" + month +
-            `\` SET \`${date}\`='${time}' where \`name\`='${name}'` +
-            ';',
-            (error, results) => {
-                return resolve(results);
-            }
-        );
-    });
-    connection.end();
-    return shift;
+async function sendShift(month, name, date, time) {
+  const connection = mysql.createConnection({
+    user: "root",
+    password: "",
+  });
+  connection.query("USE shift");
+  const shift = await new Promise((resolve, reject) => {
+    connection.query(
+      "UPDATE `" +
+        month +
+        `\` SET \`${date}\`='${time}' where \`name\`='${name}'` +
+        ";",
+      (error, results) => {
+        return resolve(results);
+      }
+    );
+  });
+  connection.end();
+  return shift;
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,194 +1,207 @@
 const week = ["日", "月", "火", "水", "木", "金", "土"];
 const today = new Date();
 let pre_date = new Date();
-let user_name = ""
+let user_name = "";
 // console.log(document.getElementsByClassName('modalClose'))
-const modal = document.getElementById('shiftModal');
-const buttonClose = document.getElementsByClassName('modalClose')[0];
+const modal = document.getElementById("shiftModal");
+const buttonClose = document.getElementsByClassName("modalClose")[0];
 
 // 月末だとずれる可能性があるため、1日固定で取得
 let showDate = new Date(today.getFullYear(), today.getMonth(), 1);
 
 // 初期表示
 window.onload = function () {
-    const url = window.location.href;
-    user_name = decodeURIComponent(url.split("user_name=")[1]);
-    showProcess(today, calendar);
+  const url = window.location.href;
+  user_name = decodeURIComponent(url.split("user_name=")[1]);
+  showProcess(today, calendar);
 };
 // 前の月表示
-function prev(){
-    showDate.setMonth(showDate.getMonth() - 1);
-    showProcess(showDate);
+function prev() {
+  showDate.setMonth(showDate.getMonth() - 1);
+  showProcess(showDate);
 }
-
 
 // 次の月表示
-function next(){
-    showDate.setMonth(showDate.getMonth() + 1);
-    showProcess(showDate);
+function next() {
+  showDate.setMonth(showDate.getMonth() + 1);
+  showProcess(showDate);
 }
-
 
 // カレンダー表示
 function showProcess(date) {
-    let year = date.getFullYear();
-    let month = date.getMonth();
-    pre_date = date;
-    document.querySelector('#header').innerHTML = year + "年 " + (month + 1) + "月";
+  let year = date.getFullYear();
+  let month = date.getMonth();
+  pre_date = date;
+  document.querySelector("#header").innerHTML =
+    year + "年 " + (month + 1) + "月";
 
-    createProcess(year, month)
-        .then((calendar) => {
-            document.querySelector('#calendar').innerHTML = calendar;
-        })
-        .catch((err) => {
-            console.log('エラーが発生しました', err);
-        });
+  createProcess(year, month)
+    .then((calendar) => {
+      document.querySelector("#calendar").innerHTML = calendar;
+    })
+    .catch((err) => {
+      console.log("エラーが発生しました", err);
+    });
 }
-
 
 // シフト編集画面表示
 function changeShift(count, shift) {
-    modal.style.display = 'block';
-    let count_date = count + "日"
-    document.querySelector('#shiftDate').innerHTML = count_date;
-    // document.querySelector('#calendar').innerHTML = calendar;
-}
+  modal.style.display = "block";
+  let count_date = count + "日";
 
+  document.querySelector("#shiftDate").innerHTML = count_date;
+  // document.querySelector('#calendar').innerHTML = calendar;
+}
 
 // シフト編集画面非表示
-buttonClose.addEventListener('click', modalClose);
+buttonClose.addEventListener("click", modalClose);
 function modalClose() {
-  modal.style.display = 'none';
+  modal.style.display = "none";
 }
 
-
 // モーダルコンテンツ以外がクリックされた時
-addEventListener('click', outsideClose);
+addEventListener("click", outsideClose);
 function outsideClose(e) {
   if (e.target == modal) {
-    modal.style.display = 'none';
+    modal.style.display = "none";
   }
 }
 
 // シフト提出
 function submit() {
-    let date = document.querySelector('#shiftDate').innerHTML
-    let selector = document.getElementById('shiftTime');
-    let index = selector.selectedIndex;
-    let time = selector.options[index].value.replace('~', '');
-    sendShift(date, time);
-    modalClose();
-    showProcess(pre_date);
+  let date = document.querySelector("#shiftDate").innerHTML;
+  let selector = document.getElementById("shiftTime");
+  let index = selector.selectedIndex;
+  let time = selector.options[index].value.replace("~", "");
+  sendShift(date, time);
+  modalClose();
+  showProcess(pre_date);
 }
-
 
 // dbから取得
 async function getCalendarFromdb(year, month) {
-    try {
-        let calendarFromdb = await axios.get('/api/getCalendar', {
-            params: {
-                'month': `${year}_${month + 1}`
-            }
-        });
+  try {
+    let calendarFromdb = await axios.get("/api/getCalendar", {
+      params: {
+        month: `${year}_${month + 1}`,
+      },
+    });
 
-        let own_shift = {};
-        let shift_per_date = {};
-        for (let i = 0; i < 31; i++) {
-            shift_per_date[`${i}`] = [];
-        }
-
-        for (const user_data of calendarFromdb.data) {
-            if (user_data["name"] === user_name) {
-                own_shift = user_data;
-            }
-            for (let i = 0; i < 31; i++) {
-                if (user_data[`${i + 1}日`] == null) {
-                    time = 0;
-                } else {
-                    time = user_data[`${i + 1}日`];
-                }
-                shift_per_date[`${i}`].push({
-                    userName: user_data["name"],
-                    time: time
-                });
-            }
-        }
-        return [own_shift, shift_per_date];
-    } catch (err) {
-        console.log(err);
+    let own_shift = {};
+    let shift_per_date = {};
+    for (let i = 0; i < 31; i++) {
+      shift_per_date[`${i}`] = [];
     }
-};
 
-
+    for (const user_data of calendarFromdb.data) {
+      if (user_data["name"] === user_name) {
+        own_shift = user_data;
+      }
+      for (let i = 0; i < 31; i++) {
+        if (user_data[`${i + 1}日`] == null) {
+          time = 0;
+        } else {
+          time = user_data[`${i + 1}日`];
+        }
+        shift_per_date[`${i}`].push({
+          userName: user_data["name"],
+          time: time,
+        });
+      }
+    }
+    return [own_shift, shift_per_date];
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 // dbに登録
 async function sendShift(date, time) {
-    try {
-        await axios.post("/api/addCalendar", {
-            params: {
-                "month": `${pre_date.getFullYear()}_${pre_date.getMonth() + 1}`,
-                "user_name": user_name,
-                "date": date,
-                "time": time
-            }
-        });
-    } catch (err) {
-        console.log(err);
-    }
-};
-
+  try {
+    await axios.post("/api/addCalendar", {
+      params: {
+        month: `${pre_date.getFullYear()}_${pre_date.getMonth() + 1}`,
+        user_name: user_name,
+        date: date,
+        time: time,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 // カレンダー作成
 async function createProcess(year, month) {
-    // 曜日
-    let calendar = "<table><tr class='dayOfWeek'>";
-    for (let i = 0; i < week.length; i++) {
-        calendar += "<th>" + week[i] + "</th>";
-    }
-    calendar += "</tr>";
-    console.log(month)
-    let count = 0;
-    let startDayOfWeek = new Date(year, month, 1).getDay();
-    let endDate = new Date(year, month + 1, 0).getDate();
-    let lastMonthEndDate = new Date(year, month, 0).getDate();
-    let row = Math.ceil((startDayOfWeek + endDate) / week.length);
-    let shift_time = "";
+  // 曜日
+  let calendar = "<table><tr class='dayOfWeek'>";
+  for (let i = 0; i < week.length; i++) {
+    calendar += "<th>" + week[i] + "</th>";
+  }
+  calendar += "</tr>";
+  console.log(month);
+  let count = 0;
+  let startDayOfWeek = new Date(year, month, 1).getDay();
+  let endDate = new Date(year, month + 1, 0).getDate();
+  let lastMonthEndDate = new Date(year, month, 0).getDate();
+  let row = Math.ceil((startDayOfWeek + endDate) / week.length);
+  let shift_time = "";
 
-    try {
-        const [own_shift, shift_per_date] = await getCalendarFromdb(year, month);
+  try {
+    const [own_shift, shift_per_date] = await getCalendarFromdb(year, month);
 
-        // 1行ずつ設定
-        for (let i = 0; i < row; i++) {
-            calendar += "<tr>";
-            // 1colum単位で設定
-            for (let j = 0; j < week.length; j++) {
-                if (i == 0 && j < startDayOfWeek) {
-                    // 1行目で1日まで先月の日付を設定
-                    calendar += "<td class='disabled'>" + (lastMonthEndDate - startDayOfWeek + j + 1) + "</td>";
-                } else if (count >= endDate) {
-                    // 最終行で最終日以降、翌月の日付を設定
-                    count++;
-                    calendar += "<td class='disabled'>" + (count - endDate) + "</td>";
-                } else {
-                    // 当月の日付を曜日に照らし合わせて設定
-                    count++;
-                    if (own_shift[`${count}日`] !== null && own_shift[`${count}日`] !== undefined) {
-                        shift_time = `${own_shift[`${count}日`]}`;
-                    } else {
-                        shift_time = "";
-                    }
-                    if (year == today.getFullYear() && month == today.getMonth() && count == today.getDate()) {
-                        calendar += `<td class='today' id='${count}'><button id='shiftOpen' type='button' onclick='changeShift(${count}, ${JSON.stringify(shift_per_date[count])})'>` + count + `</button><p>${shift_time}</p></td>`;
-                    } else {
-                        calendar += `<td id='${count}'><button id='shiftOpen' type='button' onclick='changeShift(${count}, ${JSON.stringify(shift_per_date[count])})'>` + count + `</button><p>${shift_time}</p></td>`;
-                    }
-                }
-            }
-            calendar += "</tr>";
+    // 1行ずつ設定
+    for (let i = 0; i < row; i++) {
+      calendar += "<tr>";
+      // 1colum単位で設定
+      for (let j = 0; j < week.length; j++) {
+        if (i == 0 && j < startDayOfWeek) {
+          // 1行目で1日まで先月の日付を設定
+          calendar +=
+            "<td class='disabled'>" +
+            (lastMonthEndDate - startDayOfWeek + j + 1) +
+            "</td>";
+        } else if (count >= endDate) {
+          // 最終行で最終日以降、翌月の日付を設定
+          count++;
+          calendar += "<td class='disabled'>" + (count - endDate) + "</td>";
+        } else {
+          // 当月の日付を曜日に照らし合わせて設定
+          count++;
+          if (
+            own_shift[`${count}日`] !== null &&
+            own_shift[`${count}日`] !== undefined
+          ) {
+            shift_time = `${own_shift[`${count}日`]}`;
+          } else {
+            shift_time = "";
+          }
+          if (
+            year == today.getFullYear() &&
+            month == today.getMonth() &&
+            count == today.getDate()
+          ) {
+            calendar +=
+              `<td class='today' id='${count}'><button id='shiftOpen' type='button' onclick='changeShift(${count}, ${JSON.stringify(
+                shift_per_date[count]
+              )})'>` +
+              count +
+              `</button><p>${shift_time}</p></td>`;
+          } else {
+            calendar +=
+              `<td id='${count}'><button id='shiftOpen' type='button' onclick='changeShift(${count}, ${JSON.stringify(
+                shift_per_date[count]
+              )})'>` +
+              count +
+              `</button><p>${shift_time}</p></td>`;
+          }
         }
-        return calendar;
-    } catch (err) {
-        console.log('エラーが発生しました', err);
-        throw err;
+      }
+      calendar += "</tr>";
     }
+    return calendar;
+  } catch (err) {
+    console.log("エラーが発生しました", err);
+    throw err;
+  }
 }
